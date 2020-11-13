996b2ed24f268605faab0c8ad39ce3e8fe72dcb5
Dimple13 <59231474+Dimple13@users.noreply.github.com>
2020-11-13 17:01:57 +0530

    initial upload

---
+++ src/components/Scroll.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+const Scroll=(props)=>{
+    return (
+        <div style={{overflow:'scroll',height:'800px'}}>
+            {props.children}
+        </div>
+    )
+};
+
+
+export default Scroll;
